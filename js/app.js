2ecc749db6db9e51967e36c6854dbd89eaa00225
Fransys1234 <ciocarlangalina1971@gmail.com>
2024-02-12 23:03:03 +0200

    js

---
+++ js/app.js
@@ -0,0 +1,145 @@
+// let raiting = 3;
+// console.log(raiting);
+// if (raiting === 1) {
+//   console.log("not equal");
+// } else {
+//   console.log("equal");
+// }
+
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log(`random ${random} is less`);
+// } else {
+//   console.log(`random ${random} is greater`);
+// }
+
+// let random = Math.random();
+
+// if (random > 0.4) {
+//   console.log(`random ${random} is greater`);
+// } else if (random.indexOf(0) === 0) {
+//   console.log(`random ${random} is equal`);
+// } else {
+//   console.log(`random ${random} is less`);
+// }
+
+// const age = 8;
+
+// if (age < 5) {
+//   console.log(`you are an child becouse have ${age} years`);
+// } else if (age < 10) {
+//   console.log(`you are an adult becouse have ${age} years`);
+// }
+
+// let raiting = 2;
+
+// if (raiting === 3) {
+//   console.log(`you are a super`);
+// } else if (raiting === 2) {
+//   console.log(`you are expectation`);
+// } else {
+//   console.log(`you are invalified`);
+// }
+
+// const dayOnWeek = prompt("enter a day").toLocaleLowerCase();
+
+// if (dayOnWeek === "monday") {
+//   console.log(`i am hate monday`);
+// } else if (dayOnWeek === "saturday") {
+//   console.log(`i love saturday`);
+// } else if (dayOnWeek === "friday") {
+//   console.log(`i love friday`);
+// } else {
+//   console.log(`meh`);
+// }
+
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log("your num is less than 0.5");
+// } else {
+//   console.log("your num is equal or greater than 0.5");
+// }
+// console.log(random);
+
+// const password = prompt("please enter name");
+// //pass must be 6 characters long
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("password is good");
+//   } else {
+//     console.log("password must to not contain spaces");
+//   }
+// } else {
+//   console.log("pass is less than 6");
+// }
+// pass cannot include space (-1 is no space)
+
+// const userInput = prompt("enter some password");
+
+// if (userInput) {
+//   console.log("Truthy");
+// } else {
+//   console.log("Falsey");
+// }
+
+// const password = prompt("Enter your password");
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("valid password");
+// } else {
+//   console.log("Invalid password");
+// }
+
+// const age = prompt("Enter your age");
+
+// if (age < 5 || age > 12) {
+//   if (age == 2 && age <= 2) {
+//     console.log("valid age");
+//   } else {
+//     console.log("Invalid age");
+//   }
+// }
+
+// const age = 8;
+// if (!(age >= 0 && age < 5) || age > 65) {
+//   console.log("you are not a baby or a senior");
+// }
+
+const day = 6;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("50");
+  case 6:
+  case 7:
+    console.log("weekend");
+  case 8:
+    console.log("another day");
+}
+
+// if (day === 1) {
+//   console.log("Monday");
+// }
+// if (day === 2) {
+//   console.log("Tuesday");
+// }
+// if (day === 1) {
+//   console.log("Wensday");
+// }
+// if (day === 1) {
+//     console.log("thursday");
+//   }
+//   if (day === 1) {
+//     console.log("friday");
+//   }
